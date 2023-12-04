2c15e6dd80b0cf96fa846c749b6c5dda80552837
Wilker Mizael <wilkermizael@Wilker-Macbook-Pro.local>
2023-12-04 19:55:05 -0300

    refact: deploy 1.4

--- src/Services/api.jsx
+++ src/Services/api.jsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-//const API_KEY = import.meta.env.VITE_WHEATHER_KEY;
-//const BASE_KEY = import.meta.env.VITE_BASE_KEY;
+const API_KEY = import.meta.env.VITE_WHEATHER_KEY;
+const BASE_KEY = import.meta.env.VITE_BASE_KEY;
 
 export async function findCityInOpenWeather(cityName) {
   try {
-    const response = await axios.get(`${import.meta.env.VITE_BASE_KEY}?q=${cityName}&limit=1&appid=${import.meta.env.VITE_WHEATHER_KEY}`);
+    const response = await axios.get(`${BASE_KEY}/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`);
     return response.data;
   } catch (error) {
     console.error("Erro na solicitação à API:", error);
@@ -17,7 +17,7 @@ export async function getWheather(location) {
     const lat = location[0]
     const lon = location[1]
   try {
-    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`);
+    const response = await axios.get(`${BASE_KEY}/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`);
     return response.data;
   } catch (error) {
     console.error("Erro na solicitação à API:", error);
